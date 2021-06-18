c5607af26c81538ea10d95a328bd725513502ac7
kush <kush.kalsangrah@gmail.com>
2021-06-18 17:23:33 +0530

    Updates

---
+++ precache-manifest.2d1dce1e1fb7e849f081529cd0d473cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c136a8d0917e5dcd64c236d50bc17c7b",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "37c3b53c3fd7f9773b1c",
+    "url": "/RecipeApp/static/css/main.ce2a87b8.chunk.css"
+  },
+  {
+    "revision": "8473bd66d1311660ba88",
+    "url": "/RecipeApp/static/js/2.ae21250b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.ae21250b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37c3b53c3fd7f9773b1c",
+    "url": "/RecipeApp/static/js/main.92f15b4f.chunk.js"
+  },
+  {
+    "revision": "c4fa75725029aa9c1db1",
+    "url": "/RecipeApp/static/js/runtime-main.9436be18.js"
+  }
+]);
